62de94866a05cf5cd5cc7184cae69807f74dfb32
Will Koury <wkoury@hey.com>
2022-08-11 18:32:20 -0400

    Random squawk generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    let n1: u32 = rng.gen_range(0..4095);
+
+    println!("{}", format_args!("{:04o}", n1));
 }
